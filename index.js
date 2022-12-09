1cf7ad1d75fbc9d3fc0f3fc8e50e2cd4f15cdfb7
dakhinkar <pranitdakhinkar145@gmail.com>
2022-12-09 10:59:02 +0530

    versel json file add

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 dotenv.config();
 
 // middleware
-app.use(express.static(path.join(__dirname + "/public")))
+// app.use(express.static(path.join(__dirname + "/public")))
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
